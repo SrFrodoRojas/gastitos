ab4637b79fe1a3570ff1e3146a355f723636e5ec
Cristian Dario Rojas Orue <crisrojasorue@gmail.com>
2026-06-17 19:05:59 -0300

    Backup automático 2026-06-17_19-05-57

--- app/Http/Controllers/Api/MetaAhorroController.php
+++ app/Http/Controllers/Api/MetaAhorroController.php
@@ -3,47 +3,62 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\MetaAhorroStoreRequest;
+use App\Http\Requests\MetaAhorroUpdateRequest;
+use App\Http\Resources\MetaAhorroResource;
+use App\Models\MetaAhorro;
+use Illuminate\Http\JsonResponse;
 
 class MetaAhorroController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return MetaAhorroResource::collection(
+            auth()->user()
+                ->metasAhorro()
+                ->latest()
+                ->get()
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(MetaAhorroStoreRequest $request)
     {
-        //
+        $meta = MetaAhorro::create([
+            ...$request->validated(),
+            'user_id' => auth()->id(),
+        ]);
+
+        return new MetaAhorroResource($meta);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(MetaAhorro $metaAhorro)
     {
-        //
+        $this->authorize('view', $metaAhorro);
+
+        return new MetaAhorroResource($metaAhorro);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(
+        MetaAhorroUpdateRequest $request,
+        MetaAhorro $metaAhorro
+    ) {
+        $this->authorize('update', $metaAhorro);
+
+        $metaAhorro->update(
+            $request->validated()
+        );
+
+        return new MetaAhorroResource($metaAhorro);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(MetaAhorro $metaAhorro): JsonResponse
     {
-        //
+        $this->authorize('delete', $metaAhorro);
+
+        $metaAhorro->delete();
+
+        return response()->json([
+            'message' => 'Meta eliminada correctamente'
+        ]);
     }
 }
